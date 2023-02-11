2414e1b78f323345d5b07a04c7e5294280b5a79a
翔 1078 <xiang278100598@gmail.com>
2023-02-11 10:36:07 +0800

    delete no use folder

--- ha-network-service-codebase/Business/app/utils.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from datetime import datetime
-from datetime import timedelta
-from datetime import timezone
-
-
-def get_isoformat(zone: timezone = timezone(timedelta(hours=+8))) -> str:
-    return datetime.now(zone).isoformat(timespec='milliseconds')
